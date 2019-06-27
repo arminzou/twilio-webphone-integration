8bda2d7b4baae9bd61cddbc9a5168e03e16488f3
39773496 <arminzou27@gmail.com>
2019-06-27 13:11:00 -0400

    Fix dtmf tone issue by adding the code into main javascript file

--- webphone/js/twilio-widget.js
+++ webphone/js/twilio-widget.js
@@ -269,11 +269,18 @@ function getDevice() {
                 if (!twAttr.isIncomingCall) {
                     callStatusHandler(null, 'disconnected');
                     addCallButtonListner(twAttr.iframeDocument);
+
+                    addDtmfListener();
+
+
                     clearTimeout(time);
                     clearTimer();
                 } else {
                     callStatusHandler(null, 'inc-call-end');
                     addCallButtonListner(twAttr.iframeDocument);
+
+                    addDtmfListener();
+
                     twAttr.isIncomingCall = false;
                     clearTimeout(time);
                     clearTimer();
@@ -286,6 +293,9 @@ function getDevice() {
                 console.log(`connection cancelled. Connection status: ${conn.status()}`);
                 callStatusHandler(null, 'inc-call-end');
                 addCallButtonListner(twAttr.iframeDocument);
+
+                addDtmfListener();
+
                 twAttr.isIncomingCall = false;
             });
 
@@ -354,6 +364,141 @@ function clearTimer() {
 }
 
 /*********************************************************************************
+ * DTMF TONES ON UI
+ *
+ *********************************************************************************/
+function addDtmfListener() {
+    // polyfill
+    var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
+    function Tone(context, freq1, freq2) {
+        this.context = context;
+        this.status = 0;
+        this.freq1 = freq1;
+        this.freq2 = freq2;
+    }
+
+    Tone.prototype.setup = function () {
+        this.osc1 = context.createOscillator();
+        this.osc2 = context.createOscillator();
+        this.osc1.frequency.value = this.freq1;
+        this.osc2.frequency.value = this.freq2;
+
+        this.gainNode = this.context.createGain();
+        this.gainNode.gain.value = 0.25;
+
+        this.filter = this.context.createBiquadFilter();
+        this.filter.type = "lowpass";
+        this.filter.frequency = 8000;
+
+        this.osc1.connect(this.gainNode);
+        this.osc2.connect(this.gainNode);
+
+        this.gainNode.connect(this.filter);
+        this.filter.connect(context.destination);
+    }
+
+    Tone.prototype.start = function () {
+        this.setup();
+        this.osc1.start(0);
+        this.osc2.start(0);
+        this.status = 1;
+    }
+
+    Tone.prototype.stop = function () {
+        this.osc1.stop(0);
+        this.osc2.stop(0);
+        this.status = 0;
+    }
+
+    var dtmfFrequencies = {
+        "1": {
+            f1: 697,
+            f2: 1209
+        },
+        "2": {
+            f1: 697,
+            f2: 1336
+        },
+        "3": {
+            f1: 697,
+            f2: 1477
+        },
+        "4": {
+            f1: 770,
+            f2: 1209
+        },
+        "5": {
+            f1: 770,
+            f2: 1336
+        },
+        "6": {
+            f1: 770,
+            f2: 1477
+        },
+        "7": {
+            f1: 852,
+            f2: 1209
+        },
+        "8": {
+            f1: 852,
+            f2: 1336
+        },
+        "9": {
+            f1: 852,
+            f2: 1477
+        },
+        "*": {
+            f1: 941,
+            f2: 1209
+        },
+        "0": {
+            f1: 941,
+            f2: 1336
+        },
+        "#": {
+            f1: 941,
+            f2: 1477
+        }
+    }
+
+    var context = new AudioContext();
+
+    // Create a new Tone instance. (Initialised it with 
+    // frequencies of 350 and 440 but it doesn't matter
+    // what we choose because we will be changing them at the end)
+    var dtmf = new Tone(context, 350, 440);
+
+    $(".dtmf-interface .btn-group .numpad-button", twAttr.iframeDocument).on("mousedown touchstart", function (e) {
+        e.preventDefault();
+
+        var keyPressedHtml = $(this).html(); // this gets the number/character that was pressed
+        var keyNumberPressed = keyPressedHtml.match(/\d|\#|\*/g);
+
+        var frequencyPair = dtmfFrequencies[keyNumberPressed]; // this looks up which frequency pair we need
+
+        // this sets the freq1 and freq2 properties
+        dtmf.freq1 = frequencyPair.f1;
+        dtmf.freq2 = frequencyPair.f2;
+
+        if (dtmf.status == 0) {
+            dtmf.start();
+        }
+    });
+
+    // detect the mouseup event on the window tag otherwise if we release the mouse when not over a button,
+    // the tone will remain playing
+    $(".dtmf-interface .btn-group .numpad-button", twAttr.iframeDocument).on("mouseup touchend", function () {
+        if (typeof dtmf !== "undefined" && dtmf.status) {
+            setTimeout(function () {
+                dtmf.stop();
+            }, 100);
+        }
+    });
+}
+
+
+/*********************************************************************************
  * Twilio Call UI Handlers
  *
  *********************************************************************************/
@@ -362,7 +507,6 @@ function callStatusHandler(number, status) {
         $("#dialer-card-text", twAttr.iframeDocument).html(twAttr.dialerCardHtml);
 
     } else if (status === 'connected') {
-
         $("#dialer-card-text", twAttr.iframeDocument).hide(0).html(
             `<h1 class="text-center" id="code">Calling</h1>
                         <span>${number}</span>
@@ -631,6 +775,8 @@ function widgetController() {
             }
 
             addCallButtonListner(twAttr.iframeDocument);
+            addDtmfListener();
+
             twAttr.dialerCardHtml = $("#dialer-card-text", twAttr.iframeDocument).html();
 
 
